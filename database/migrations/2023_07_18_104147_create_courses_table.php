b458566c67fbb38aa44008063af4a2c6af63e918
mxguru <135461260+mxguru@users.noreply.github.com>
2023-07-24 02:53:11 +0400

    table update

--- database/migrations/2023_07_18_104147_create_courses_table.php
+++ database/migrations/2023_07_18_104147_create_courses_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string("name");
+            $table->longText("image");
             $table->double("price");
             $table->string("description");
             $table->string("coach");
