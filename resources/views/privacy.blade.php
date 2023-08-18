084a09b4fd3b28d97febbd0e60448e97c6818294
Lewin Muzvonda <muzvondalewin@gmail.com>
2023-08-19 01:51:50 +0400

    update

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout> --}}
     <div class="pt-4 bg-gray-100">
         <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
           
@@ -52,4 +52,4 @@
             </div>
         </div>
     </div>
-</x-guest-layout>
+{{-- </x-guest-layout> --}}
